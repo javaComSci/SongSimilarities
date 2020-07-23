d61c3d79180d0ab5c2835050dc002d366aafeeb7
javaComSci <iramanat@purdue.edu>
2020-07-22 20:18:37 -0400

    Framework for model

--- Model.py
+++ Model.py
@@ -7,109 +7,206 @@ import numpy as np
     Create the model that will be trained for the word embeddings
 """
 class Model:
-    def __init__(self):
-        self.context_inputs = None
-        self.word_outputs = None
+    def __init__(self, vocab_size, hidden_layers, batch_size, inputs, outputs):
+        # sizes necessary for computation
+        self.vocab_size = vocab_size
+        self.hidden_layers = hidden_layers
+        self.batch_size = batch_size
+
+        # number of examples to train on
+        self.examples_size = inputs.shape[1]
+
+        # training data for model
+        self.inputs = inputs
+        self.outputs = outputs
+
+        # vectors necessary for computation
+        self.weights_1 = None
+        self.weights_2 = None
+        self.bias_1 = None
+        self.bias_2 = None
+    
+
+
+    """
+        Creates the relu function for computation
+    """
+    def relu(self, m):
+        # print(m)
+        return np.maximum(0, m)
+    
+
+
+    """
+        Creates the softmax function for computation
+    """
+    def softmax(self, m):
+        return np.exp(m)/np.sum(np.exp(m), axis = 0)
 
 
 
     """
-        Create the vector representation of the input data
+        Initialize all the matricies and vectors in order to train the model
     """
-    def vectorize(self, vocab, slide_size = 2, verbose = False):
-        vectorizer = Vectorizer()
+    def initialize(self):
+        # print("INITIALIZATION")
 
-        # create the vector for single words as the outputs
-        one_hot_vectors = vectorizer.one_hot_vectorize(vocab)
+        # input layer to hidden layer
+        self.weights_1 = np.random.rand(self.hidden_layers, self.vocab_size)
+        self.bias_1 = np.random.rand(self.hidden_layers, 1)
+    
+        # hidden layer to output layer
+        self.weights_2 = np.random.rand(self.vocab_size, self.hidden_layers)
+        self.bias_2 = np.random.rand(self.vocab_size, 1)
 
-        if verbose == True:
-            print("SHAPE OF VECTORS CREATED: ", one_hot_vectors.shape)
 
-        # get the tokens for each song
-        song_artist_lyrics_df = pd.read_pickle("SongArtistLyricsTokenized.pkl")
 
-        if verbose == True:
-            print("SONG ARTIST LYRICS DATAFRAME: ")
-            print(song_artist_lyrics_df.head(1))
+    """
+        Compute the cost of using the current weights
+    """
+    def get_cost(self, predictions, batch_outputs):
+
+        # check for shape matching
+        assert (predictions.shape == batch_outputs.shape), "Predictions and outputs do not match"
         
+        # calculate the cost with cross entropy
+        logprobs = np.multiply(np.log(predictions),batch_outputs) + np.multiply(np.log(1 - predictions), 1 - batch_outputs)
+        cost = - 1/self.batch_size * np.sum(logprobs)
+        cost = np.squeeze(cost)
 
-        first_overall = True
+        return cost
 
-        # create the input context and output word for each of the words
-        for index, row in song_artist_lyrics_df.iterrows():
-            
-            # temp stack for specific song
-            first_addition = True
-            temp_inputs = None
-            temp_outputs = None
 
-            if verbose == True:
-                print(index)
-            
-            tokens = row["Lyrics"]
 
-            # create the sliding window for each word to obtain context
-            for i in range(slide_size, len(tokens) - slide_size):
-                # obtain the word and the context
-                word = tokens[i]
-                context = tokens[i - slide_size: i] + tokens[i + 1: i + slide_size + 1]
+    """
+        Calculate the value of the prediction given the inputs and weights
+    """
+    def forward_propogate(self, batch_inputs):
+        # print("FORWARD PROPOGATE")
 
-                # obtain the vectorized representation
-                vector_output = vectorizer.get_one_hot_vector(word)
-                vector_input = vectorizer.get_averager_vector(context)
+        # calculate relu(W1x + b1)
+        weights_1_vocab_bias = self.relu(np.dot(self.weights_1, batch_inputs) + self.bias_1)
+        assert (weights_1_vocab_bias.shape[0] == self.hidden_layers), "Addition of weights and bias incorrect 1"
 
-                # if verbose == True:
-                #     print("CONTEXT: ", context)
-                #     print("INPUT: ", vector_input)
-                #     print("WORD: ", word)
-                #     print("OUTPUT: ", vector_output)
-            
-                # add to the list of inputs and outputs
-                if first_addition == True:
-                    first_addition = False
-                    temp_inputs = vector_input
-                    temp_outputs = vector_output
-                else:
-                    temp_inputs = np.hstack((temp_inputs, vector_input))
-                    temp_outputs = np.hstack((temp_outputs, vector_output))
-            
+        # calculate softmax(W2*relu(W1x + b1) + b2)
+        weights_2_vocab_bias = self.softmax(np.dot(self.weights_2, weights_1_vocab_bias) + self.bias_2)
+        assert (weights_2_vocab_bias.shape[0] == self.vocab_size), "Addition of weights and bias incorrect 2"
 
-            if first_overall == True:
-                first_overall = False
-                self.context_inputs = temp_inputs
-                self.word_outputs = temp_outputs
-            else:
-                self.context_inputs = np.hstack((self.context_inputs, temp_inputs))
-                self.word_outputs  = np.hstack((self.word_outputs, temp_outputs))
-                print("CONTEXT INPUTS SHAPE: ", self.context_inputs.shape)
-                print("WORD OUTPUTS SHAPE: ", self.word_outputs.shape)
-                    
+        return weights_2_vocab_bias, weights_1_vocab_bias
+
+
+
+    """
+        Calculate the updates to the weights and bias vectors
+    """
+    def backward_propogate(self, batch_inputs, batch_outputs, predictions, relu_results, alpha = 0.05):
+        # print("BACKWARD PROPOGATE")
+
+        # compute the necessary gradients for each of the weights and biases
+        l1 = np.dot(self.weights_2.T, predictions - batch_outputs)
+        l1 = np.maximum(l1, 0)
+        grad_W1 = np.dot(l1, batch_inputs.T)/self.batch_size
+        grad_W2 = np.dot(predictions - batch_outputs, relu_results.T)/self.batch_size
+        grad_b1 = np.dot(l1, np.ones((self.batch_size, 1)))/self.batch_size
+        grad_b2 = np.dot(predictions - batch_outputs, np.ones((self.batch_size, 1)))/self.batch_size
+    
+        assert(self.weights_1.shape == grad_W1.shape), "Weights and gradients shape not same 1"
+        assert(self.weights_2.shape == grad_W2.shape), "Weights and gradients shape not same 2"
+        assert(self.bias_1.shape == grad_b1.shape), "Bias and gradients shape not same 1"
+        assert(self.bias_2.shape == grad_b2.shape), "Bias and gradients shape not same 2"
+
+        # update the bias and weight vectors
+        self.weights_1 = self.weights_1 - (alpha * grad_W1) 
+        self.weights_2 = self.weights_2 - (alpha * grad_W2)
+        self.bias_1 = self.bias_1 - (alpha * grad_b1)
+        self.bias_2 = self.bias_2 - (alpha * grad_b2)
+
+
+    
+    """
+        Evaluate the model with the current weights and outputs
+    """
+    def evaluate(self):
+
+        # get the predictions
+        predictions, relu_results = self.forward_propogate(inputs)
+
+        # get the actual predictions
+        max_columns_predictions = np.argmax(predictions, axis=0)
+
+        # get the best columns in the outputs
+        max_columns_outputs = np.argmax(outputs, axis = 0)
         
-        if verbose == True:
-            print("CONTEXT INPUTS SHAPE: ", self.context_inputs.shape)
-            print("WORD OUTPUTS SHAPE: ", self.word_outputs.shape)
-            # print(np.nonzero(self.context_inputs[:,0]))
-            # print(self.context_inputs[:,0].shape)
+        assert(max_columns_outputs.shape == max_columns_predictions.shape), "Outputs and predictons not same length"
+        print("SHAPE IS: ", max_columns_predictions.shape[0])
+
+        # get the correct ones
+        correct = 0
+        for i in range(max_columns_predictions.shape[0]):
+            print(max_columns_predictions[i], max_columns_outputs[i])
+            if max_columns_predictions[i] == max_columns_outputs[i]:
+                correct += 1
+
+        # calculate the accuracy
+        accuracy = (correct/max_columns_predictions.shape[0])
+        print("ACCURACY", accuracy)
+
+
+
+    """
+        Train the model with the inputs and outputs
+    """
+    def train(self, times_to_repeat):
+        alpha = 0.05
+        for j in range(times_to_repeat):
+
+            self.evaluate()
+
+            # reduce the learning rate
+            alpha = alpha * 0.5
+
+            # train by applying forward and backward propogation
+            for i in range(0, self.examples_size - self.batch_size, self.batch_size):
+
+                # get current inputs and outputs to train on
+                batch_inputs = self.inputs[:,i: i + batch_size]
+                batch_outputs = self.outputs[:, i : i + batch_size]
+
+                # get the predictions through forward propogation
+                predictions, relu_results = self.forward_propogate(batch_inputs)
+                
+
+                # display the cost
+                if i % 1000 == 0:
+                    cost = self.get_cost(predictions, batch_outputs)
+                    print("ITERATIONS: ", i)
+                    print("COST: ", cost)
+                
+                # do back propogation to update the weights
+                self.backward_propogate(batch_inputs, batch_outputs, predictions, relu_results, alpha)
         
-
-        # save the inputs and outputs as a pickle
-        np.save("ContextInputs", self.context_inputs)
-        np.save("WordOutputs", self.word_outputs)
-
 
 
 
 if __name__ == "__main__":
-    # get the data necessary to train the model
-    vocab = None
-    with open("Vocabulary.pkl", 'rb') as handle:
-        vocab = pickle.load(handle)
+    # obtain the inputs and outputs from the saved numpy files
+    inputs = np.load("ContextInputs.npy")
+    outputs = np.load("WordOutputs.npy")
 
-    # create model 
-    model = Model()
+    # create the model with given parameters
+    vocab_size = 4350
+    hidden_layers = 2
+    batch_size = 50
 
-    # get the inputs and the output vectors for the model
-    slide_size = 2
-    model.vectorize(vocab, slide_size, True)
+    # create a model
+    model = Model(vocab_size, hidden_layers, batch_size, inputs, outputs)
 
+    # initialize all the matricies for computation
+    model.initialize()
 
+    # train the model
+    times_to_repeat = 5
+    model.train(times_to_repeat)
+
+    # evauate the model
+    model.evaluate()
--- Vectorizer.py
+++ Vectorizer.py
@@ -11,6 +11,8 @@ class Vectorizer():
         self.index_to_word = {}
         self.one_hot_vectors = None
         self.vocab_size = 0
+        self.context_inputs = None
+        self.word_outputs = None
 
 
     """
@@ -59,3 +61,102 @@ class Vectorizer():
         # create the average of the embeddings in the context
         average = total/len(context)
         return average
+
+    
+
+    """
+        Create the vector representation of the input data
+    """
+    def vectorize(self, vocab, slide_size = 2, verbose = False):
+
+        # create the vector for single words as the outputs
+        one_hot_vectors = self.one_hot_vectorize(vocab)
+
+        if verbose == True:
+            print("SHAPE OF VECTORS CREATED: ", one_hot_vectors.shape)
+
+        # get the tokens for each song
+        song_artist_lyrics_df = pd.read_pickle("SongArtistLyricsTokenized.pkl")
+
+        if verbose == True:
+            print("SONG ARTIST LYRICS DATAFRAME: ")
+            print(song_artist_lyrics_df.head(1))
+        
+
+        first_overall = True
+
+        # create the input context and output word for each of the words
+        for index, row in song_artist_lyrics_df.iterrows():
+            
+            # temp stack for specific song
+            first_addition = True
+            temp_inputs = None
+            temp_outputs = None
+
+            if verbose == True:
+                print(index)
+            
+            tokens = row["Lyrics"]
+
+            # create the sliding window for each word to obtain context
+            for i in range(slide_size, len(tokens) - slide_size):
+                # obtain the word and the context
+                word = tokens[i]
+                context = tokens[i - slide_size: i] + tokens[i + 1: i + slide_size + 1]
+
+                # obtain the vectorized representation
+                vector_output = self.get_one_hot_vector(word)
+                vector_input = self.get_averager_vector(context)
+
+                # if verbose == True:
+                #     print("CONTEXT: ", context)
+                #     print("INPUT: ", vector_input)
+                #     print("WORD: ", word)
+                #     print("OUTPUT: ", vector_output)
+            
+                # add to the list of inputs and outputs
+                if first_addition == True:
+                    first_addition = False
+                    temp_inputs = vector_input
+                    temp_outputs = vector_output
+                else:
+                    temp_inputs = np.hstack((temp_inputs, vector_input))
+                    temp_outputs = np.hstack((temp_outputs, vector_output))
+            
+
+            if first_overall == True:
+                first_overall = False
+                self.context_inputs = temp_inputs
+                self.word_outputs = temp_outputs
+            else:
+                self.context_inputs = np.hstack((self.context_inputs, temp_inputs))
+                self.word_outputs  = np.hstack((self.word_outputs, temp_outputs))
+                print("CONTEXT INPUTS SHAPE: ", self.context_inputs.shape)
+                print("WORD OUTPUTS SHAPE: ", self.word_outputs.shape)
+                    
+        
+        if verbose == True:
+            print("CONTEXT INPUTS SHAPE: ", self.context_inputs.shape)
+            print("WORD OUTPUTS SHAPE: ", self.word_outputs.shape)
+            # print(np.nonzero(self.context_inputs[:,0]))
+            # print(self.context_inputs[:,0].shape)
+        
+
+        # save the inputs and outputs as a pickle
+        np.save("ContextInputs", self.context_inputs)
+        np.save("WordOutputs", self.word_outputs)
+
+
+
+if __name__ == "__main__":
+    # get the data necessary to train the model
+    vocab = None
+    with open("Vocabulary.pkl", 'rb') as handle:
+        vocab = pickle.load(handle)
+
+    # create vectorizer 
+    vectorizer = Vectorizer()
+
+    # get the inputs and the output vectors for the model
+    slide_size = 2
+    vectorizer.vectorize(vocab, slide_size, True)
